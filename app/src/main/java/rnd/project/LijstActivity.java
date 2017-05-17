81f4b74f550c5860278685912f014f8ed5d060ff
Kushmeister420 <joeyvandeneijnden@hotmail.com>
2017-05-18 00:05:50 +0200

    LijstActivity met ingevulde list.

--- app/src/main/java/rnd/project/LijstActivity.java
+++ app/src/main/java/rnd/project/LijstActivity.java
@@ -1,17 +1,23 @@
 package rnd.project;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 
 /**
  * Created by Gebruiker on 17/05/2017.
  */
 
-public class LijstActivity extends AppCompatActivity {
+public class LijstActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lijst);
+
+        String[] geld = {"200,-", "50,-", "18,88", "21,22"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getListView().getContext(), android.R.layout.simple_list_item_1, geld);
+        getListView().setAdapter(adapter);
     }
 }
