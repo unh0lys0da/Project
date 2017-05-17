5f23fc234a818517a42c8d2532bdd8c7ad1fe84e
TomvdWaa <tvanderwaa@tudelft.nl>
2017-05-17 22:16:51 +0200

    SetUpCharts() en FillCharts() als het goed is af.

--- app/src/main/java/rnd/project/MainActivity.java
+++ app/src/main/java/rnd/project/MainActivity.java
@@ -1,6 +1,7 @@
 package rnd.project;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,11 @@ import android.view.View;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,19 +26,24 @@ public class MainActivity extends AppCompatActivity {
     float[] uitgavenData = {200, 100, 300, 50}; //TestSet
     public PieChart pieChartInkomsten;
     public PieChart pieChartUitgaven;
+    private ArrayList<Integer> colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addColors();
         setUpCharts();
+    }
 
-
-
-
-
-
+    private void addColors() {
+        colors.add(Color.GRAY);
+        colors.add(Color.BLUE);
+        colors.add(Color.DKGRAY);
+        colors.add(Color.CYAN);
+        colors.add(Color.MAGENTA);
+        //Zijn voorlopig maar 5 kleuren, kan altijd makkelijk meer doen, wil ook liever hex-based kleuren gaan gebruiken.
     }
 
     public void gotoInkomsten(View v){
@@ -81,7 +91,52 @@ public class MainActivity extends AppCompatActivity {
         descUitgaven.setText("Uitgaven per categorie in €"); //Dan zo de text setten
 
         pieChartInkomsten.setDescription(descInkomsten);
-        pieChartUitgaven.setDescription(descUitgaven);
+        pieChartUitgaven.setDescription(descUitgaven); //En ze dan zo aan een pieChart toevoegen
+
+        //Wat kleine grafische dingetjes
+        pieChartInkomsten.setHoleRadius(0);
+        pieChartUitgaven.setHoleRadius(0); //Lelijk gat in het midden van een piechart uitgezet
+
+        fillCharts(); //DataSets toevoegen
+
+    }
+
+    private void fillCharts() {
+        //Dit stuk voorzie ik nog van comments in de nabije toekomst
+        ArrayList<PieEntry> yEntriesInkomsten = new ArrayList<>();
+        ArrayList<PieEntry> yEntriesUitgaven = new ArrayList<>();
+        ArrayList<String> xEntriesInkomsten = new ArrayList<>();
+        ArrayList<String> xEntriesUitgaven = new ArrayList<>();
+
+        //Deze prachtige forloops vullen de getallen in beide charts, had gekund met foreach loops,
+        //maar dat werkt slordig omdat je ook een index mee wil geven, die het makkelijkst via i meegegeven wordt.
+        for(int i = 0; i < inkomstenData.length; i++) { yEntriesInkomsten.add(new PieEntry(inkomstenData[i], i)); } //Getallen
+        for(int i = 0; i < categoryInkomsten.length; i++) {xEntriesInkomsten.add(categoryInkomsten[i]); } //Categorie-naam
+        for(int i = 0; i < uitgavenData.length; i++) { yEntriesUitgaven.add(new PieEntry(uitgavenData[i], i)); } //Getallen
+        for(int i = 0; i < categoryUitgaven.length; i++) {xEntriesUitgaven.add(categoryUitgaven[i]); } //Categorie-naam
+
+
+        //Maken van de DataSets
+        PieDataSet inkomstenDataSet = new PieDataSet(yEntriesInkomsten, "Bedrag per categorie");
+        inkomstenDataSet.setSliceSpace(0);
+        inkomstenDataSet.setValueTextSize(14);
+        inkomstenDataSet.setColors(colors);
+        PieDataSet uitgavenDataSet = new PieDataSet(yEntriesUitgaven, "Bedrag per categorie");
+        uitgavenDataSet.setSliceSpace(0);
+        uitgavenDataSet.setValueTextSize(14);
+        uitgavenDataSet.setColors(colors);
+
+        //Maken van PieData (Vraag me niet waarom dit zo lomp is)
+        PieData inkomstenPieData = new PieData(inkomstenDataSet);
+        PieData uitgavenPieData = new PieData(uitgavenDataSet);
+
+        //Toevoegen van de Data aan de charts
+        pieChartInkomsten.setData(inkomstenPieData);
+        pieChartUitgaven.setData(uitgavenPieData);
+
+        //Invalidate de charts
+        pieChartInkomsten.invalidate();
+        pieChartUitgaven.invalidate();
 
     }
 }
