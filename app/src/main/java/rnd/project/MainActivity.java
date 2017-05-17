ba81743cc7252f1a257329a55b3b0986384f87d3
TomvdWaa <tvanderwaa@tudelft.nl>
2017-05-17 21:43:36 +0200

    Charts in Javabestand werken krijgen dmv functie SetUpCharts()

--- app/src/main/java/rnd/project/MainActivity.java
+++ app/src/main/java/rnd/project/MainActivity.java
@@ -5,12 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    public List<Entry> entriesInkomsten; //DataSet voor Linker Grafiek
+    public List<Entry> entriesUitgaven; //DataSet voor rechtergrafiek.
+    String[] categoryInkomsten = {"Lening", "Ouders", "Werk", "etc"}; //TestSet
+    String[] categoryUitgaven = {"Eten", "Uitgaan", "Huur", "etc"}; //TestSet
+    float[] inkomstenData = {400, 200, 300, 20}; //TestSet
+    float[] uitgavenData = {200, 100, 300, 50}; //TestSet
+    public PieChart pieChartInkomsten;
+    public PieChart pieChartUitgaven;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpCharts();
+
+
+
+
+
+
     }
 
     public void gotoInkomsten(View v){
@@ -46,9 +69,19 @@ public class MainActivity extends AppCompatActivity {
     public void gotoHome(View v){
         setContentView(R.layout.lijst);
     }
-    //Douwe is een flikkert.
-    //klopt, groetjes rits
-    //test, groetjes Auke
-    //ook ff een test
-    //Ik vind Douwe best oke hoor.
+
+    private void setUpCharts() {
+        PieChart pieChartInkomsten = (PieChart) findViewById(R.id.PieChartLinks);
+        PieChart pieChartUitgaven = (PieChart) findViewById(R.id.PieChartRechts);
+
+        Description descInkomsten = new Description();
+        Description descUitgaven = new Description(); //Maak de descriptions eerst zo aan
+
+        descInkomsten.setText("Inkomsten per categorie in €");
+        descUitgaven.setText("Uitgaven per categorie in €"); //Dan zo de text setten
+
+        pieChartInkomsten.setDescription(descInkomsten);
+        pieChartUitgaven.setDescription(descUitgaven);
+
+    }
 }
