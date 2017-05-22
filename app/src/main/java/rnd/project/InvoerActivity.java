6b6a21e148aa45e95db7d4fcedd9d65d119316da
Niels-vv <nielsvv@msn.com>
2017-05-22 21:39:55 +0200

    Extended InvoerActivity with database function as an example

--- app/src/main/java/rnd/project/InvoerActivity.java
+++ app/src/main/java/rnd/project/InvoerActivity.java
@@ -1,17 +1,34 @@
 package rnd.project;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by Gebruiker on 17/05/2017.
  */
 
 public class InvoerActivity extends AppCompatActivity {
-
+    DatabaseHelper db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nieuwe_invoer);
+        db = new DatabaseHelper(this);
+        readCategorie();
+    }
+
+    //Read categorie column from database and give toast of categorie for each row
+    public void readCategorie (){
+        Cursor data = db.readCat();
+        int ColumnToShow = 0;
+        while (data.moveToNext()) {
+            toastMessage(data.getString(ColumnToShow));
+        }
+    }
+
+    private void toastMessage (String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
