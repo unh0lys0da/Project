18ce114360a52a67efe5ddd412fbba6d04942b5f
Kushmeister420 <joeyvandeneijnden@hotmail.com>
2017-05-23 16:49:18 +0200

    imports klopten niet.

--- app/src/main/java/rnd/project/InkomstenActivity.java
+++ app/src/main/java/rnd/project/InkomstenActivity.java
@@ -5,7 +5,19 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Gebruiker on 17/05/2017.
