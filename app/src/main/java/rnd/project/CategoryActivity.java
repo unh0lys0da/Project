aaea58dda079d530360a173c3079e074ea95e9b8
Niels-vv <nielsvv@msn.com>
2017-06-01 14:03:36 +0200

    categorieActivity (en de XML) aangepast zodat hij de al toegevoegde categorieën laat zien

--- app/src/main/java/rnd/project/CategoryActivity.java
+++ app/src/main/java/rnd/project/CategoryActivity.java
@@ -5,18 +5,35 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class CategoryActivity extends AppCompatActivity {
 
     // Deze klasse kan vanuit 'Settings' worden aangeroepen. Hier kan de gebruiker een categorie toevoegen.
-
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
+        listView = (ListView) findViewById(R.id.listCat);
+        setupListView(listView);
+    }
+
+    //Laat de al toegevoegde categorieën zien in de scrollview
+    public void setupListView (ListView listview) {
+        DatabaseHelper db;
+        db = new DatabaseHelper(this);
+        Cursor data = db.getCategories(); //Haalt de categorieën uit de database
+        String[] catArray = new String[data.getCount()];
+        for (int i = 0; data.moveToNext(); i++) {
+            catArray[i] = data.getString(0);
+        }
+        ArrayAdapter<String> listArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, catArray);
+        listview.setAdapter(listArrayAdapter);
     }
 
     public void removeCategory(View v) {
@@ -46,6 +63,8 @@ public class CategoryActivity extends AppCompatActivity {
     private void toastMessage (String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
+
 
     /*
     private void hernoemCategorie () {
